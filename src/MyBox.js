37463d81ab367bc15e98c240d6f2c4dd87137b33
Alexis Molina <alexismoga8@gmail.com>
2019-05-08 16:43:17 +0200

    Update MyBox.js

--- src/MyBox.js
+++ src/MyBox.js
@@ -1,56 +1,38 @@
- 
 class MyBox extends THREE.Mesh {
   constructor() {
     super();
     
     // Se crea la parte de la interfaz que corresponde a la caja
     // Se crea primero porque otros métodos usan las variables que se definen para la interfaz
-    this.createGUI();
     
-    // Un Mesh se compone de geometría y material
-    this.geometry = new THREE.BoxGeometry (1,1,1);
-    // Las primitivas básicas se crean centradas en el origen
-    // Se puede modificar su posición con respecto al sistema de coordenadas local con una transformación aplicada directamente a la geometría.
-    this.geometry.applyMatrix (new THREE.Matrix4().makeTranslation(0,0.5,0));
-    // Como material se crea uno a partir de un color
-    this.material = new THREE.MeshPhongMaterial({color: 0xCF0000});
-  }
-  
-  createGUI () {
-    // Controles para el tamaño, la orientación y la posición de la caja
-    this.guiControls = new function () {
-      this.sizeX = 1.0;
-      this.sizeY = 1.0;
-      this.sizeZ = 1.0;
-      
-      // Un botón para dejarlo todo en su posición inicial
-      // Cuando se pulse se ejecutará esta función.
-      this.reset = function () {
-        this.sizeX = 1.0;
-        this.sizeY = 1.0;
-        this.sizeZ = 1.0;
-      }
-    } 
+    //Gepmetria taza fuera
+    this.recorridoSpline = new THREE.CatmullRomCurve3([
+      new THREE.Vector3( 0, 10, - 10 ), new THREE.Vector3( 10, 0, - 10 ),
+			new THREE.Vector3( 20, 0, 0 ), new THREE.Vector3( 30, 0, 10 ),
+			new THREE.Vector3( 30, 0, 20 ), new THREE.Vector3( 20, 0, 30 ),
+			new THREE.Vector3( 10, 0, 30 ), new THREE.Vector3( 0, 0, 30 ),
+			new THREE.Vector3( - 10, 10, 30 ), new THREE.Vector3( - 10, 20, 30 ),
+			new THREE.Vector3( 0, 30, 30 ), new THREE.Vector3( 10, 30, 30 ),
+			new THREE.Vector3( 20, 30, 15 ), new THREE.Vector3( 10, 30, 10 ),
+			new THREE.Vector3( 0, 30, 10 ), new THREE.Vector3( - 10, 20, 10 ),
+			new THREE.Vector3( - 10, 10, 10 ), new THREE.Vector3( 0, 0, 10 ),
+			new THREE.Vector3( 10, - 10, 10 ), new THREE.Vector3( 20, - 15, 10 ),
+			new THREE.Vector3( 30, - 15, 10 ), new THREE.Vector3( 40, - 15, 10 ),
+			new THREE.Vector3( 50, - 15, 10 ), new THREE.Vector3( 60, 0, 10 ),
+			new THREE.Vector3( 70, 0, 0 ), new THREE.Vector3( 80, 0, 0 ),
+			new THREE.Vector3( 90, 0, 0 ), new THREE.Vector3( 100, 0, 0 )
+    ]);
     
-    // Se crea una sección para los controles de la caja
-    var folder = gui.addFolder ('Dimensiones de la caja');
-    // Estas lineas son las que añaden los componentes de la interfaz
-    // Las tres cifras indican un valor mínimo, un máximo y el incremento
-    // El método   listen()   permite que si se cambia el valor de la variable en código, el deslizador de la interfaz se actualice
-    folder.add (this.guiControls, 'sizeX', 0.1, 5.0, 0.1).name ('Tamaño X : ').listen();
-    folder.add (this.guiControls, 'sizeY', 0.1, 5.0, 0.1).name ('Tamaño Y : ').listen();
-    folder.add (this.guiControls, 'sizeZ', 0.1, 5.0, 0.1).name ('Tamaño Z : ').listen();
-    
-    folder.add (this.guiControls, 'reset').name ('[ Reset ]');
+    this.recorridoSpline = this.recorridoSpline.getPoints(100);
+    this.opciones = {steps: 100, curveSegments: 50, extrudePath: this.recorridoSpline};
+    this.geometry = new THREE.TubeGeometry(this.recorridoSpline, 20,2,8, false);
+    this.material = new THREE.MeshPhongMaterial( { color : 0xff0000 } );
+
+    //this.mesh = new THREE.Mesh(this.geometry, this.material);
+
   }
   
   update () {
-    // Con independencia de cómo se escriban las 3 siguientes líneas, el orden en el que se aplican las transformaciones es:
-    // Primero, el escalado
-    // Segundo, la rotación en Z
-    // Después, la rotación en Y
-    // Luego, la rotación en X
-    // Y por último la traslación
-    this.scale.set (this.guiControls.sizeX,this.guiControls.sizeY,this.guiControls.sizeZ);
+   
   }
 }
